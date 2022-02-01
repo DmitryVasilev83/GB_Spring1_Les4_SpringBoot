809b56a54f6f2bfa06c087f148998bdc1ad34653
VasilevDmitry83 <VasilevDmitry83@mail.ru>
2022-02-01 15:33:57 +0300

    main_les4

---
+++ les4/les4/src/main/java/ru/geekbrains/les4/Les4Application.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.les4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Les4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Les4Application.class, args);
+	}
+
+}
